e5ae2c2920a0f01c00776ae47373b719386a39b0
Ziyang Yuan <yuanzy.brandon2011@gmail.com>
2024-11-21 11:33:32 -0600

    7.1 beta test

--- TestPlugin/MyHook.cs
+++ TestPlugin/MyHook.cs
@@ -10,10 +10,10 @@ namespace TestPlugin;
 
 public unsafe class MyHook : IDisposable
 {
-    private delegate void OnLoadingHitbox(GameObject* gameObject, float unkownVal);
-    private delegate void OnUpdatingHitbox(GameObject* gameObject);
+    private delegate void OnLoadingHitbox(ulong unkownAddr, float unscaledRadius);
+    //private delegate void OnUpdatingHitbox(GameObject* gameObject);
     private Hook<OnLoadingHitbox>? _onHitboxLoadedHook;
-    private Hook<OnUpdatingHitbox>? _onHitboxUpdatedHook;
+    //private Hook<OnUpdatingHitbox>? _onHitboxUpdatedHook;
 
     public static void Initialize() { Instance = new MyHook(); }
 
@@ -23,7 +23,7 @@ public unsafe class MyHook : IDisposable
         try
         {
             _onHitboxLoadedHook = Plugin.GameInteropProvider.HookFromSignature<OnLoadingHitbox>(
-                    "48 89 5C 24 08 57 48 83 EC 20 48 8B D9 E8 ?? ?? ?? ?? 48 8D 8B A0 01",
+                    "40 53 48 83 EC 20 48 8B D9 48 8B 49 08 66 83 B9 78 06 00 00 00 74 29",
                     OnHitboxLoaded
                 );
 
@@ -34,18 +34,18 @@ public unsafe class MyHook : IDisposable
             Plugin.Logger.Error("Error initiating OnloadingHitbox hooks: " + e.Message);
         }
 
-        try
-        {
-            _onHitboxUpdatedHook = Plugin.GameInteropProvider.HookFromSignature<OnUpdatingHitbox>(
-                    "40 53 48 83 EC 20 66 83 B9 98 06 00 00 00",
-                    onHitboxUpdated
-                );
-                _onHitboxUpdatedHook?.Enable();
-        }
-        catch (Exception e)
-        {
-            Plugin.Logger.Error("Error initiating OnUpdatingHitbox hooks: " + e.Message);
-        }
+        //try
+        //{
+        //    _onHitboxUpdatedHook = Plugin.GameInteropProvider.HookFromSignature<OnUpdatingHitbox>(
+        //            "40 53 48 83 EC 20 66 83 B9 98 06 00 00 00",
+        //            onHitboxUpdated
+        //        );
+        //        _onHitboxUpdatedHook?.Enable();
+        //}
+        //catch (Exception e)
+        //{
+        //    Plugin.Logger.Error("Error initiating OnUpdatingHitbox hooks: " + e.Message);
+        //}
 
     }
 
@@ -79,18 +79,25 @@ public unsafe class MyHook : IDisposable
         _onHitboxLoadedHook?.Disable();
         _onHitboxLoadedHook?.Dispose();
 
-        _onHitboxUpdatedHook?.Disable();
-        _onHitboxLoadedHook?.Dispose();
+        //_onHitboxUpdatedHook?.Disable();
+        //_onHitboxUpdatedHook?.Dispose();
 
     }
 
-    private unsafe void OnHitboxLoaded(GameObject* gameObject, float unkownVal)
+    private unsafe void OnHitboxLoaded(ulong unknownAddr, float unscaledRadius)
     {
-        this._onHitboxLoadedHook?.Original(gameObject, unkownVal);
+        this._onHitboxLoadedHook?.Original(unknownAddr, unscaledRadius);
 
         try
         {
-            gameObject->HitboxRadius += 3.0f;
+            //Plugin.Logger.Info("Pointer:" + unknownAddr + "radius: " + unscaledRadius);
+            GameObject** gameObjectPtr = (GameObject**)(unknownAddr + 0x8);
+            GameObject* gameObject = *gameObjectPtr;
+            float* hitboxRadius = (float*)((ulong)gameObject + 0xD0);
+            //Plugin.Logger.Info("Character Pointer: " + ((ulong)gameObjectPtr).ToString("X8") + " Character: " + ((ulong)gameObject).ToString("X8") + " radius: " + *hitboxRadius);
+            //gameObject->HitboxRadius += 3.0f;
+            *hitboxRadius += 3.0f;
+            Plugin.Logger.Info("Character Pointer: " + ((ulong)gameObjectPtr).ToString("X8") + " Character: " + ((ulong)gameObject).ToString("X8") + " radius: " + *hitboxRadius);
         }
         catch (Exception ex)
         {
@@ -98,17 +105,17 @@ public unsafe class MyHook : IDisposable
         }
     }
 
-    private unsafe void onHitboxUpdated(GameObject* gameObject)
-    {
-        this._onHitboxUpdatedHook?.Original(gameObject);
+    //private unsafe void onHitboxUpdated(GameObject* gameObject)
+    //{
+    //    this._onHitboxUpdatedHook?.Original(gameObject);
 
-        try
-        {
-            gameObject->HitboxRadius += 3.0f;
-        }
-        catch (Exception ex)
-        {
-            Plugin.Logger.Error(ex, "An error occured when editing hitbox during hitbox updating.");
-        }
-    }
+    //    try
+    //    {
+    //        gameObject->HitboxRadius += 3.0f;
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Plugin.Logger.Error(ex, "An error occured when editing hitbox during hitbox updating.");
+    //    }
+    //}
 }
